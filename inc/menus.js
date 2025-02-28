d6e7ef6418ef7ac113dc767e16b61bea42f311bc
Andryll Silva <andryll.silva@outlook.com>
2025-02-27 22:34:55 -0300

    185. MY24 - Administração do Menu - Enviando Novo Menu via POST

--- inc/menus.js
+++ inc/menus.js
@@ -1,4 +1,5 @@
 let conn = require('./db');
+var path = require('path')
 
 module.exports = {
   getMenus() {
@@ -14,6 +15,31 @@ module.exports = {
 
         resolve(results)
       })
+    })
+  },
+
+  save(fields, files){
+
+    fields.photo = `images/${path.paser(files.photo.path).base}`
+
+    return new Promise((resolve, reject) => {
+
+      conn.query(`
+          INSERT INTO tb_menus (title, description, price, photo)
+          VALUES(?, ?, ?, ?)
+          `, [
+            fields.title,
+            fields.description,
+            fields.price,
+            fields.photo
+          ], (err, results) => {
+
+            if(err){
+              reject(err)
+            }else {
+              resolve(results)
+            }
+          })
     })
   }
 }
